027b0bb11602d45d35b83e044d4b4d48695232ab
voeurnchy <voeurnchipse@gmail.com>
2018-10-01 11:58:08 +0700

    custom homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Voeurn Chy
                 </div>
 
                 <div class="links">
